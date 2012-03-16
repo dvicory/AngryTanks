17e1f636ffb89ebcf08f9f371b8b9fbbc22d1628
Daniel Vicory <dvicory@gmail.com>
2012-03-15 19:49:58 -0700

    commit initial VS projects

---
+++ AngryTanks.Client/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace AngryTanks.Client
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (AngryTanks game = new AngryTanks())
+            {
+                game.Run();
+            }
+        }
+    }
+}
+
