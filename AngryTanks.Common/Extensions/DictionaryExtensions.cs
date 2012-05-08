7738740e90e51c30cee9315969039ef95b30fd32
Daniel Vicory <dvicory@gmail.com>
2012-05-07 20:23:59 -0700

    Add Dictionary extensions.

---
+++ AngryTanks.Common/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AngryTanks.Common.Extensions
+{
+    namespace DictionaryExtensions
+    {
+        public static class DictionaryExtensionsClass
+        {
+            public static void RemoveAll<TKey, TValue>(this IDictionary<TKey, TValue> dict, Func<KeyValuePair<TKey, TValue>, bool> predicate)
+            {
+                List<TKey> keysToRemove = new List<TKey>();
+
+                foreach (KeyValuePair<TKey, TValue> kvp in dict.Where(predicate))
+                    keysToRemove.Add(kvp.Key);
+
+                foreach (TKey key in keysToRemove)
+                    dict.Remove(key);
+            }
+        }
+    }
+}
